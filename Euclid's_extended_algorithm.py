2541c73e9739a1c46f1e2efa842d6b2b693a0654
sohamsamanta2000 <53594493+sohamsamanta2000@users.noreply.github.com>
2020-09-21 01:04:58 +0530

    Euclid's algorithm
    algorithms and mathematical explanations

---
+++ Euclid's_extended_algorithm.py
@@ -0,0 +1,33 @@
+def ext_gcd(a, b):
+    x, y = 0, 1  # initial values as explained below
+    x1, y1 = 1, 0  # initial values as explained below
+    while a != 0:
+        q = b // a  # quotient at each step of euclid's algorithm
+        r = b % a  # remainder at each step of euclid's algorithm
+        x2 = x - x1 * q  # updating x_i as explained below  x_i=(x_i-2)-(x_i-1)(q_i)
+        y2 = y - y1 * q  # updating y_i as explained below  y_i=(y_i-2)-(y_i-1)(q_i)
+        b = a  # updating b
+        a = r  # updating a
+        x = x1  # (x_i-2)=(x_i-1) updated by next value
+        y = y1  # (y_i-2)=(y_i-1) updated by next value
+        x1 = x2  # (x_i-1)=(x_i)   updated by next value
+        y1 = y2  # (x_i-1)=(x_i)   updated by next value
+
+    gcd = b
+
+    return gcd, x, y
+
+
+print(ext_gcd(10889992, 66))  # example
+
+# Explanation of the algorithm (Mathematical approach)
+# We can represent each step of the euclid's algorithm by : r_i=(q_i+2)*(r_i+1)+(r_i+2),
+#                                                           where (q_i+2)=[(r_i)/(r_i+1)], [] is gif
+#                                                           also r_0 =a and r_1=b (defined)
+#  So, we can write r_i=(r_i-2)-(q_i)*(r_i-1), 0<= r_i < (r_i-1)  ---------------(1)
+#  Now, we can write r_i =(x_i)*a + (y_i)*b
+#  putting this in the recursive relation (1), above, we get: x_i=(x_i-2)-(x_i-1)(q_i)
+#                                                        and  y_i=(y_i-2)-(y_i-1)(q_i)
+# now for the initial values, : a=r_0=(x_0)*a + (y_0)*b , so x_0=1 and y_0=0
+#                             : b=r_1=(x_1)*a + (y_1)*b , so x_1=0 and y_1=0
+#  here gcd(a,b)=r_n-2=(x_n-2)*a + (y_n-2)*b
---
+++ Euclid_algorithm.py
@@ -0,0 +1,28 @@
+def gcd(a,b):
+    a=max(a,b)  
+    b=min(a,b)
+
+    if b==0:       #base case
+        return a   
+    else:
+
+        c=a        #temporary variable
+        a=b       
+        b=c%b    #updating b by, r_i+2 as explained below
+
+        return gcd(a,b)
+
+print( gcd(200,66)) #example
+
+
+#algorithm explanation
+# check whether b=0 or not 
+        #this is will happen at the last step of euclid's algorithm, i.e when the nth remainder is zero (here b=nth remainder)
+             #in this case a is the gcd
+#else 
+        #store b's value in a and the ith remainder in b (i.e. b=a%b)
+        # then pass these modified values of a and b to the gcd function for further iteration.
+
+#We can represent each step of the euclid's algorithm by : r_i=(q_i+2)*(r_i+1)+(r_i+2), 
+#                                                           where (q_i+2)=[(r_i)/(r_i+1)], [] is gif
+#                                                           also r_0 =a and r_1=b (defined)
